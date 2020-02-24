82053706b6764c326d7598b8719a4edd60b9451c
Mona Lisa <patrinoua@gmail.com>
2020-02-24 23:13:36 +0200

    add filter and reset button

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,6 +1,14 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-import React from "react";
-import { RefreshControl, ScrollView, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import {
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+  View,
+  TextInput,
+  Text,
+  Button
+} from "react-native";
 import { Appbar, DataTable, FAB } from "react-native-paper";
 import {
   NavigationEvents,
@@ -11,16 +19,17 @@ import { useSelector, useDispatch } from "react-redux";
 import { selectors, actions } from "./store/inventory";
 import { RootState } from "./store";
 
-
 export default (props: NavigationInjectedProps) => {
   const fetching = useSelector((state: RootState) => state.inventory.fetching);
   const inventory = useSelector(selectors.selectInventory);
   const dispatch = useDispatch();
+  const [filterValue, setFilterValue] = useState("");
 
   return (
     <View style={{ flex: 1 }}>
-      <NavigationEvents onWillFocus={() => dispatch(actions.fetchInventory())} />
-
+      <NavigationEvents
+        onWillFocus={() => dispatch(actions.fetchInventory())}
+      />
       <Appbar.Header>
         <Appbar.Content title="Inventory" />
       </Appbar.Header>
@@ -40,17 +49,30 @@ export default (props: NavigationInjectedProps) => {
               <DataTable.Title>Product Code</DataTable.Title>
               <DataTable.Title numeric>Scan Date</DataTable.Title>
             </DataTable.Header>
-            {inventory.map((record, index) => (
-              <DataTable.Row key={index}>
-                <DataTable.Cell>
-                  {record.fields["Product Code"]}
-                </DataTable.Cell>
-                <DataTable.Cell numeric>
-                  {new Date(record.fields.Posted).toLocaleDateString()}{" "}
-                  {new Date(record.fields.Posted).toLocaleTimeString()}
-                </DataTable.Cell>
-              </DataTable.Row>
-            ))}
+            <TextInput
+              style={styles.textInput}
+              placeholder={"Filter by Code:"}
+              onChangeText={value => setFilterValue(value)}
+              value={filterValue}
+            />
+            <Button title={"Reset"} onPress={() => setFilterValue("")} />
+            {inventory.map((record, index) => {
+              if (record.fields["Product Code"].includes(filterValue)) {
+                return (
+                  <DataTable.Row key={index}>
+                    {/* Index as a key is an anti pattern. Objects should have a unique identifier.
+                     https://medium.com/@robinpokorny/index-as-a-key-is-an-anti-pattern-e0349aece318 */}
+                    <DataTable.Cell>
+                      {record.fields["Product Code"]}
+                    </DataTable.Cell>
+                    <DataTable.Cell numeric>
+                      {new Date(record.fields.Posted).toLocaleDateString()}{" "}
+                      {new Date(record.fields.Posted).toLocaleTimeString()}
+                    </DataTable.Cell>
+                  </DataTable.Row>
+                );
+              }
+            })}
           </DataTable>
         </SafeAreaView>
       </ScrollView>
@@ -58,11 +80,7 @@ export default (props: NavigationInjectedProps) => {
       <SafeAreaView style={styles.fab}>
         <FAB
           icon={() => (
-            <MaterialCommunityIcons
-              name="barcode"
-              size={24}
-              color="#0B5549"
-            />
+            <MaterialCommunityIcons name="barcode" size={24} color="#0B5549" />
           )}
           label="Scan Product"
           onPress={() => props.navigation.navigate("Camera")}
@@ -70,7 +88,7 @@ export default (props: NavigationInjectedProps) => {
       </SafeAreaView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   fab: {
@@ -79,5 +97,11 @@ const styles = StyleSheet.create({
     width: "100%",
     flex: 1,
     alignItems: "center"
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: "lightgrey",
+    padding: 5,
+    margin: 5
   }
 });
